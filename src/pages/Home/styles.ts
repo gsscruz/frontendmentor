2a5ee1e586d472fb7d0b923445103f8bb10b477c
Gustavo <gmonte@tutanota.com>
2022-12-25 05:49:10 -0800

    add github pages config

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -21,30 +21,30 @@ export const ImageContainer = styled.div`
   border-radius: 10px;
   width: 19.2rem;
   height: 19.2rem;
-  background-image: url('/src/assets/image-qr-code.png') ;
+  background-image: url('/src/image/image-qr-code.png');
   background-repeat: no-repeat;
   background-position: center;
   background-size: 100%;
   overflow: hidden;
-margin-bottom: 1.6rem;
+  margin-bottom: 1.6rem;
 `;
 
 export const TextContainer = styled.div`
-display: flex;
-flex-direction: column;
-text-align: center;
-align-items: center;
-justify-content: center;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Title = styled.h1`
-color:${props => props.theme['dark-blue']};
-font-size: 1.46rem;
-margin-bottom: 1rem;
+  color: ${(props) => props.theme['dark-blue']};
+  font-size: 1.46rem;
+  margin-bottom: 1rem;
 `;
 export const SubTitle = styled.p`
   color: ${(props) => props.theme['grayish-blue']};
   font-size: 1rem;
   font-weight: 400;
-  width:17em
+  width: 17em;
 `;
